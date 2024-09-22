12fdb0110406e20e4ebe7fda2535dff76417a826
triisyavelly <67626450+triisyavelly@users.noreply.github.com>
2024-09-22 17:58:16 +0700

    soal2w2

---
+++ soal2w2.js
@@ -0,0 +1,49 @@
+
+let productBin = {
+    "requestId": null,
+    "data": [
+      {
+        "id": 100000057465,
+        "storageId": 10000008207,
+        "code": "A01-01-01-A",
+        "productId": 110000081009,
+        "productName": "FloBrand-DressBSPink",
+        "productCode": "FBR00040101",
+        "quantity": 76,
+        "createdTime": "2021-12-21T13:54:48Z",
+      },
+      {
+        "id": 100000057466,
+        "storageId": 10000002181,
+        "code": "A01-01-01-B",
+        "productId": 110000081009,
+        "productName": "FloBrand-DressBSPink",
+        "productCode": "FBR00040101",
+        "quantity": 71,
+        "createdTime": "2021-12-21T13:54:48Z",
+      },
+      {
+        "id": 100000065224,
+        "storageId": 10000008884,
+        "code": "Tgt00-A-A-01",
+        "productId": 110000081009,
+        "productName": "FloBrand-DressBSPink",
+        "productCode": "FBR00040101",
+        "quantity": 10,
+        "createdTime": "2022-02-08T10:35:19Z",
+      }
+    ],
+    "message": "success"
+  }
+
+function penjumlahan(productBin) {
+    let temp = 0
+    let data = productBin.data
+    for (let i=0; i< data.length; i++) {
+        temp = temp + data[i].quantity
+    }
+    return temp
+}
+const result= penjumlahan (productBin)
+console.log(result)
+
